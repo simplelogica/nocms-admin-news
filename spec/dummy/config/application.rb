c3b02ff499fd2beb7e3a07c40546f0c4e64635e5
David J. Brenes <david.brenes@simplelogica.net>
2014-06-05 16:11:08 +0200

    Removing wrong requiring gem

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "no_cms-admin-news"
 
 module Dummy
   class Application < Rails::Application
